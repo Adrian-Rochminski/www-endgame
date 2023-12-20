a2cfdaad2500f552fd6357919c536c06f2ff1678
Adrian-Rochminski <235959@edu.p.lodz.pl>
2023-12-20 13:59:15 +0100

    Pocztkowy commit mojego projektu

---
+++ backend/website/__init__.py
@@ -0,0 +1,58 @@
+from flask import Flask
+from pymongo import MongoClient, server_api
+from flask_jwt_extended import JWTManager
+
+app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'www-secret-key-to-jwt-tokens'
+jwt = JWTManager(app)
+MONGODB_URI = "mongodb+srv://Adrian:Korsarz29@parkinglotdatabase.urd7nin.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(MONGODB_URI, server_api=server_api.ServerApi('1'))
+db = client['parkinglotdatabase']
+users_collection = db['users']
+parkings_collection = db['parkings']
+
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+
+##data example
+
+# user_id = ObjectId()
+# users_collection.insert_one({
+#     '_id': user_id,
+#     'username': 'jan_kowalski',
+#     'password': 'secretPassword123',
+#     'license_plates': ['DW12345', 'DW67890'],
+#     'is_owner': True,
+#     'money': 21.37
+# })
+#
+# parking_id = ObjectId()
+# parkings_collection.insert_one({
+#     '_id': parking_id,
+#     'name': 'Central Parking',
+#     'available_spots': [
+#         {'number': 1, 'is_free': True, 'current_car': None, 'occupancy_date': None},
+#         {'number': 2, 'is_free': False, 'current_car': 'DW12345', 'occupancy_date': datetime.now()}
+#     ],
+#     'night_hours': {'from': '22:00', 'to': '06:00'},
+#     'day_hours': {'from': '06:00', 'to': '22:00'},
+#     'night_rate': 5.0,
+#     'day_rate': 10.0,
+#     'history': [
+#         {'license_plate': 'DW67890', 'start_date': datetime.now(), 'end_date': None, 'paid': 0.0}
+#     ]
+# })
+
+def create_app():
+    app = Flask(__name__)
+
+    from .views import views
+
+    app.register_blueprint(views, url_prefix='/')
+    app.config['JWT_SECRET_KEY'] = 'www-secret-key-to-jwt-tokens'
+    jwt = JWTManager(app)
+    return app
---
+++ backend/website/views.py
@@ -0,0 +1,124 @@
+from flask import Blueprint, jsonify, request, make_response
+from flask_jwt_extended import create_access_token, jwt_required
+from werkzeug.security import generate_password_hash, check_password_hash
+from website import db
+import uuid
+
+views = Blueprint('views', __name__)
+users_collection = db['users']
+parkings_collection = db['parkings']
+
+
+@views.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+
+    if not data or not data.get('username') or not data.get('password'):
+        return make_response('Invalid data', 400)
+
+    if users_collection.find_one({'username': data['username']}):
+        return make_response('User already exists', 400)
+
+    hashed_password = generate_password_hash(data['password'], method='sha256')
+
+    new_user = {
+        '_id': str(uuid.uuid4()),
+        'username': data['username'],
+        'password': hashed_password,
+        'license_plates': [],
+        'is_owner': False,
+        'money': 0.0
+    }
+
+    users_collection.insert_one(new_user)
+
+    return jsonify({'message': 'User has been registered'}), 201
+
+
+@views.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+
+    if not data or not data.get('username') or not data.get('password'):
+        return make_response('Invalid data', 400)
+
+    user = users_collection.find_one({'username': data['username']})
+
+    if user and check_password_hash(user['password'], data['password']):
+        access_token = create_access_token(identity=data['username'])
+        return jsonify(access_token=access_token, user=user['_id']), 200
+    else:
+        return make_response('Bad username or password', 401)
+
+
+@views.route('/user/<user_id>/license_plate', methods=['PUT'])
+@jwt_required()
+def add_license_plate(user_id):
+    data = request.get_json()
+    new_plate = data['license_plate']
+
+    result = users_collection.update_one(
+        {'_id': user_id, 'license_plates': {'$ne': new_plate}},
+        {'$push': {'license_plates': new_plate}}
+    )
+
+    if result.modified_count:
+        return jsonify({'message': 'License plate added successfully'}), 200
+    else:
+        return jsonify({'message': 'License plate already exists or user not found'}), 400
+
+
+@views.route('/user/<user_id>/license_plate', methods=['DELETE'])
+@jwt_required()
+def remove_license_plate(user_id):
+    data = request.get_json()
+    plate_to_remove = data['license_plate']
+
+    result = users_collection.update_one(
+        {'_id': user_id},
+        {'$pull': {'license_plates': plate_to_remove}}
+    )
+
+    if result.modified_count:
+        return jsonify({'message': 'License plate removed successfully'}), 200
+    else:
+        return jsonify({'message': 'License plate not found or user not found'}), 400
+
+
+@views.route('/user/<user_id>/license_plates', methods=['GET'])
+@jwt_required()
+def get_license_plates(user_id):
+    user = users_collection.find_one({'_id': user_id}, {'license_plates': 1, '_id': 0})
+
+    if user:
+        return jsonify({'license_plates': user.get('license_plates', [])}), 200
+    else:
+        return jsonify({'message': 'User not found'}), 404
+
+
+@views.route('/user/<user_id>/update_license_plate', methods=['POST'])
+@jwt_required()
+def update_license_plate(user_id):
+    data = request.get_json()
+    old_plate = data['old_license_plate']
+    new_plate = data['new_license_plate']
+
+    result = users_collection.update_one(
+        {'_id': user_id, 'license_plates': old_plate},
+        {'$set': {'license_plates.$': new_plate}}
+    )
+
+    if result.modified_count:
+        return jsonify({'message': 'License plate updated successfully'}), 200
+    else:
+        return jsonify({'message': 'Old license plate not found or user not found'}), 400
+
+
+@views.route('/users', methods=['GET'])
+def get_all_users():
+    users = users_collection.find()
+    users_list = []
+    for user in users:
+        user['_id'] = str(user['_id'])
+        users_list.append(user)
+    return jsonify(users_list)
