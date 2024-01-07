9bbb3d5638412f6e42c86bfbea9f5041813f485e
Krizzuu <kskrzysiek788@gmail.com>
2024-01-07 22:17:44 +0100

    added car parking, made a function to check if a car has been parked

--- backend/website/parking.py
+++ backend/website/parking.py
@@ -6,6 +6,7 @@ from website import db
 
 parking = Blueprint('parking', __name__)
 parking_collection = db['parkings']
+user_collection = db['users']
 
 
 def parse_str_to_time(time_str):
@@ -14,6 +15,16 @@ def parse_str_to_time(time_str):
 
 def parse_time_to_str(time):
     return time.strftime("%H:%M")
+
+
+def has_car_parked(plate):
+    query = {"current_usage": {"$elemMatch": {"car": plate}}}
+    results = parking_collection.find(query)
+    return len(list(results)) > 0
+
+
+def check_car_ownership(plate, user_id):
+    return plate in user_collection.find_one({"_id": user_id})["license_plates"]
 
 
 @parking.route("/make", methods=["POST"])
@@ -81,22 +92,75 @@ def create_parking():
         "day_time_end": parse_time_to_str(data["day_time_end"])
     }
 
-    result = parking_collection.insert_one(new_parking)
+    parking_collection.insert_one(new_parking)
 
-    if result.modified_count:
-        return jsonify({"msg": "New parking added"})
-    else:
-        return jsonify({'message': "Error saving a new parking"}), 400
-
+    return jsonify({"msg": "New parking added"}), 201
 
 
 @parking.route("/check_plate/<plate>", methods=["GET"])
 @jwt_required()
 def check_car_status(plate):
-    query = {"current_usage": {"$elemMatch": {"car": plate}}}
-    results = parking_collection.find(query)
+    user_id = get_jwt_identity()
 
-    if len(list(results)):
+    if not check_car_ownership(plate, user_id):
+        return jsonify({"msg": "User does not have a car with given license plate"}), 400
+
+    if has_car_parked(plate):
         return jsonify({"msg": "The car is parked"}), 200
     else:
         return jsonify({"msg": "The car is not parked"}), 200
+
+
+@parking.route("/park/<plate>", methods=["GET"])
+@jwt_required()
+def park_on_cheapest(plate):
+    if has_car_parked(plate):
+        return jsonify({"msg": "The car has already parked"}), 400
+
+    user_id = get_jwt_identity()
+
+    if not check_car_ownership(plate, user_id):
+        return jsonify({"msg": "User does not have a car with given license plate"}), 400
+
+    parking_lots = parking_collection.find({})
+
+    current_time = datetime.now().time()
+
+    cheapest_rate = float('inf')
+    cheapest_lot = None
+
+    for lot in parking_lots:
+        day_start = parse_str_to_time(lot['day_time_start'])
+        day_end = parse_str_to_time(lot['day_time_end'])
+
+        if day_start <= current_time <= day_end:
+            rate = lot['day_rate']
+        else:
+            rate = lot['night_rate']
+
+        if any(spot['available'] for spot in lot['spots']):
+            if rate < cheapest_rate:
+                cheapest_rate = rate
+                cheapest_lot = lot
+
+    if cheapest_lot:
+        for spot in cheapest_lot['spots']:
+            if spot['available']:
+                spot['available'] = False
+
+                current_usage_update = {
+                    "floor": spot['floor'],
+                    "spot": spot['spot'],
+                    "car": plate,
+                    "start_time": parse_time_to_str(current_time),
+                    "end_time": None
+                }
+
+                parking_collection.update_one(
+                    {"_id": cheapest_lot['_id']},
+                    {"$push": {"current_usage": current_usage_update},
+                     "$set": {"spots": cheapest_lot['spots']}}
+                )
+                return jsonify({"msg": "The car is successfully parked"}), 200
+    else:
+        return jsonify({"msg": "There is no parking available"}), 404
