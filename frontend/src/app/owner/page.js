21f4eb338aa79a00eb499de51eb6eeb1f2d5daec
anekil <78557681+anekil@users.noreply.github.com>
2024-01-29 14:59:24 +0100

    fixed owner page

--- frontend/src/app/owner/page.js
+++ frontend/src/app/owner/page.js
@@ -1,13 +1,17 @@
 // pages/owner/page.js
 import React from 'react';
-import PageClient from './page.client';
+import {Owner} from './page.client';
+import {getServerSession} from "next-auth";
+import {authOptions} from "@/lib/auth";
 
-const Page = () => {
-  return (
-    <div>
-      <PageClient />
-    </div>
-  );
+const Page = async () => {
+    const session = await getServerSession(authOptions)
+
+    return (
+        <div>
+            <Owner {...session} />
+        </div>
+    );
 };
 
 export default Page;
