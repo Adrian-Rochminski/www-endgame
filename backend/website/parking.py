dc3e7b551e20f26d6663a5822af0de5f4c2ea0c8
Krizzuu <kskrzysiek788@gmail.com>
2024-02-06 23:46:34 +0100

    hotfix

--- backend/website/parking.py
+++ backend/website/parking.py
@@ -363,10 +363,11 @@ def search_parking():
     parkings = parking_collection.find({"address": {"$regex": parking_name, "$options": "i"}})
     # parking_list = [{"id": str(parking["_id"]), "address": parking["address"]} for parking in parkings]
     parking_list = []
-    for p in parking:
+    for p in parkings:
         p.pop('current_usage')
         p.pop('history')
-        p.pop('costs')
+        if 'costs' in p:
+            p.pop('costs')
         parking_list.append(p)
 
     if parking_list:
